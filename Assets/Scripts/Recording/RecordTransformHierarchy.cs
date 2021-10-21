42beea650e565e49b1799b0de1e5d87bc4abf4cc
Jonathan <jonathan@opuscope.com>
2021-10-21 23:14:17 +0200

    Got animation recording and playback working using new and legacy

--- Assets/Scripts/Recording/RecordTransformHierarchy.cs
+++ Assets/Scripts/Recording/RecordTransformHierarchy.cs
@@ -1,43 +1,59 @@
 using UnityEngine;
+
 #if UNITY_EDITOR
 using UnityEditor.Animations;
 #endif
 
 public class RecordTransformHierarchy : MonoBehaviour
 {
-    [SerializeField]
-    private AnimationClip _clip;
+    [SerializeField] private AnimationClip _clip;
+
+    [SerializeField] private float _period;
 
 #if UNITY_EDITOR
-    private GameObjectRecorder _Recorder;
-
-    void Start()
+    
+    private GameObjectRecorder _recorder;
+    private float _currentTime;
+    
+    void OnEnable()
     {
         // Create recorder and record the script GameObject.
-        _Recorder = new GameObjectRecorder(gameObject);
+        _recorder = new GameObjectRecorder(gameObject);
 
         // Bind all the Transforms on the GameObject and all its children.
-        _Recorder.BindComponentsOfType<Transform>(gameObject, true);
+        _recorder.BindComponentsOfType<Transform>(gameObject, true);
+
+        _currentTime = 0f;
     }
 
     void LateUpdate()
     {
         if (!_clip)
+        {
             return;
+        }
 
-        // Take a snapshot and record all the bindings values for this frame.
-        _Recorder.TakeSnapshot(Time.deltaTime);
+        _currentTime += Time.deltaTime;
+
+        if (_currentTime > _period)
+        {
+            // Take a snapshot and record all the bindings values for this frame.
+            _recorder.TakeSnapshot(_currentTime);
+            _currentTime -= _period;
+        }
     }
 
     void OnDisable()
     {
         if (_clip == null)
+        {
             return;
+        }
 
-        if (_Recorder.isRecording)
+        if (_recorder.isRecording)
         {
             // Save the recorded session to the clip.
-            _Recorder.SaveToClip(_clip);
+            _recorder.SaveToClip(_clip);
         }
     }
 #endif

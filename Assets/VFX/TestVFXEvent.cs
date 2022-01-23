4b02530a490c44ca28a843fedaee74173b47b138
jonathan-opuscope <jonathan@opuscope.com>
2022-01-24 00:01:11 +0100

    Working on hits lifetime

---
+++ Assets/VFX/TestVFXEvent.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+namespace Sonosthesia
+{
+    [CustomEditor(typeof(TestVFXEvent))]
+    public class TestEventEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            TestVFXEvent testVFXEvent = (TestVFXEvent)target;
+            if(GUILayout.Button("Send"))
+            {
+                testVFXEvent.SendEvent();
+            }
+        }
+    }    
+}
+
+#endif
+
+namespace Sonosthesia
+{
+    public class TestVFXEvent : MonoBehaviour
+    {
+        [SerializeField] private VisualEffect _visualEffect;
+        
+        [SerializeField] private string _eventName;
+
+        [SerializeField] private float _range = 1f;
+
+        [SerializeField] private float _size = 1f;
+        
+        [SerializeField] private float _lifetime = 1f;
+        
+        [SerializeField] [ColorUsage(true, true)] private Color _color = Color.white;
+
+        public void SendEvent()
+        {
+            Debug.Log($"Send event : {_eventName}");
+
+            Vector3 position = this.transform.position + Random.insideUnitSphere * _range;
+        
+            VFXEventAttribute eventAttribute = _visualEffect.CreateVFXEventAttribute();
+        
+            eventAttribute.SetVector3("color", new Vector3(_color.r, _color.g, _color.b));
+            eventAttribute.SetVector3("position", position);
+            eventAttribute.SetFloat("size", _size);
+            eventAttribute.SetFloat("lifetime", _lifetime);
+        
+            _visualEffect.SendEvent(_eventName, eventAttribute);
+        }
+    }    
+}
+
+
